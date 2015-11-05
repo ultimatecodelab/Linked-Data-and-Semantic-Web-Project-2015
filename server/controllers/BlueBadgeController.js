b3ec408790b300f79b991819c6468d20c765c439
Arjun Kharel <adgn353@gmail.com>
2015-11-05 13:52:49 +0000

    Created controller for bluebadge model
    Have created the controller for bluebadge model...

---
+++ server/controllers/BlueBadgeController.js
@@ -0,0 +1,19 @@
+// taking a mongoose model and converts it into a REST API/ builds all the CRUD operations
+var restful = require('node-restful');
+
+module.exports = function(app, route) {
+
+  // Setting up the controller for REST...based on the meters model which will have DEL, PUT, POST, GET
+  var rest = restful.model(
+    'bluebadge',
+    app.models.bluebadge
+  ).methods(['get', 'put', 'post', 'delete']);
+
+  // Register this REST API with the application at specific routes.
+  rest.register(app, route);
+
+  // Return middleware...to  provide custom features per controller basis
+  return function(req, res, next) {
+    next();
+  };
+};
