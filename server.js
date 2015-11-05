944071b2c2bfd31c02afc277877bce9833e22a28
jamesngondo2013 <jamesngondo2013@outlook.com>
2015-11-05 13:01:14 +0000

    index file added
    index file added and node modules installed

--- server.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by Arjun-Kharel on 27/10/2015.
- */
---
+++ server/index.js
@@ -0,0 +1,34 @@
+
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var _ = require('lodash');
+
+// Create the application.
+var app = express();
+
+// Add Middleware necessary for REST API's
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(methodOverride('X-HTTP-Method-Override'));
+
+// CORS Support allowing other servers to access our API
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type'); //opening accessibilty
+  next();
+});
+
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost/galwaycityparking');
+
+mongoose.connection.once('open', function() {
+
+
+
+  console.log('Listening on port 3000...');
+  app.listen(3000);
+});
