a3f77e9651162b009807b23f99bf0b05d2341381
jamesngondo2013 <jamesngondo2013@outlook.com>
2015-11-05 13:38:53 +0000

    Bluebadge and Meters models
    Added Bluebadge.js and Meters.js models to the models folder...defining
    the Schema

---
+++ server/models/Bluebadge.js
@@ -0,0 +1,38 @@
+//models are only for defining schemas for the database
+var mongoose = require('mongoose');
+
+// Create the Parking Schema.
+var BluebadgeSchema = new mongoose.Schema({
+  id: {
+    type: String
+  },
+  location: {
+    type: String
+  },
+  no_spaces: {
+    type: String
+  },
+  description: {
+      type: String  
+  },
+  street: {
+      type: String
+  },
+  lat: {
+    type: String
+  },
+  long: {
+    type: String
+  },
+  eastitm: {
+    type: String
+  },
+  northitm: {
+    type: String
+  },
+  eastig: {
+    type: String
+  },
+  northig: {
+    type: String
+  }
---
+++ server/models/Meters.js
@@ -0,0 +1,25 @@
+//models are only for defining schemas for the database
+var mongoose = require('mongoose');
+
+// Create the Parking Schema.
+var MeterSchema = new mongoose.Schema({
+  id: {
+    type: String
+  },
+  machineid: {
+    type: String
+  },
+  location: {
+    type: String
+  },
+  lat: {
+    type: String
+  },
+  long: {
+    type: String
+  }
+});
+
+// Export the model schema when one requires it.
+module.exports = MeterSchema;
+
