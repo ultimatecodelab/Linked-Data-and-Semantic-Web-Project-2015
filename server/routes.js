eec44a1fcc1963da8641ee628befce34fb3562ee
Arjun Kharel <adgn353@gmail.com>
2015-11-05 15:34:18 +0000

    bug fixes

--- server/routes.js
+++ server/routes.js
@@ -1,7 +1,6 @@
 
 module.exports = {
  '/parking': require('./controllers/parkingcontroller'),
- '/movie': require('./controllers/MovieController'),
  '/meters': require('./controllers/MeterController'),
  '/bluebadge': require('./controllers/BlueBadgeController')
     
