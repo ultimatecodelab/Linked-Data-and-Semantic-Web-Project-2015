706891f057729d89771912d8c762f33dcb6caedd
Arjun Kharel <adgn353@gmail.com>
2015-11-05 14:10:41 +0000

    Created routes.js for linking controllers
    Create the routes.js file which has all the routes for linking
    controllers...

---
+++ server/routes.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+ '/parking': require('./controllers/parkingcontroller'),
+ '/movie': require('./controllers/MovieController'),
+ '/meters': require('./controllers/MeterController'),
+ '/bluebadge': require('./controllers/BlueBadgeController')
+    
+};
